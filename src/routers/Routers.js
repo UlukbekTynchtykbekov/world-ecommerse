b48f458110420d69a96b1120d64a0bc558860e0c
bekturb <bektursunbaimamatov@MacBook-Pro-Bektursun.local>
2023-06-16 20:41:10 +0600

    added loader and some root of css

--- src/routers/Routers.js
+++ src/routers/Routers.js
@@ -2,12 +2,16 @@ import React from 'react';
 import {Route, Routes} from "react-router-dom";
 import Register from "../pages/Register";
 import Otp from "../pages/Otp";
+import Login from "../pages/Login";
 
 const Routers = () => {
-    return <Routes>
-        <Route path="/" element={<Register/>}/>
-        <Route path="/otp" element={<Otp/>}/>
-    </Routes>
+    return (
+        <Routes>
+            <Route path="/register" element={<Register/>}/>
+            <Route path="/otp" element={<Otp/>}/>
+            <Route path="/login" element={<Login/>}/>
+        </Routes>
+    )
 };
 
 export default Routers;
