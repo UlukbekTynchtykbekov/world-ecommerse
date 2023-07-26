5be39a50fd189a5a284a9fc6a51b5e5f60f6c388
bekturb <bekkgboy2@gmail.com>
2023-07-26 12:13:49 +0600

    started checkout page

--- src/routers/Routers.js
+++ src/routers/Routers.js
@@ -8,6 +8,7 @@ import SinglePage from "../pages/SinglePage";
 import OfferPage from "../pages/OfferPage";
 import CategoryPage from "../pages/CategoryPage";
 import Cart from "../pages/Cart";
+import Checkout from "../pages/Checkout";
 
 const Routers = () => {
     return (
@@ -18,6 +19,7 @@ const Routers = () => {
             <Route path="/offer-page" element={<OfferPage/>}/>
             <Route path="/offer-page" element={<OfferPage/>}/>
             <Route path="/cart-page" element={<Cart/>}/>
+            <Route path="/checkout-page" element={<Checkout/>}/>
             <Route path="/otp" element={<Otp/>}/>
             <Route path="/login" element={<Login/>}/>
         </Routes>
